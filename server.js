a13ac750f3b97dcfb87cc267984c38fbe1b770c1
trsalondra <trsalondra@gmail.com>
2022-11-30 13:11:57 -0600

    bottle countdown created

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send(`99 Bottles of beer on the wall<br><a href = http://localhost:${port}/98>take one down, pass it around</a> `)
+    // res.put(bottleNum--)
+})
+
+app.get('/0', (req, res) => {
+    res.send(`0 Bottles of beer on the wall<br>`)
+}) 
+
+app.get('/:bottleNum', (req, res) => {
+    let nextBottle = req.params.bottleNum - 1
+    res.send(`${req.params.bottleNum} Bottles of beer on the wall<br><a href = http://localhost:${port}/${nextBottle}>take one down, pass it around</a> `)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
