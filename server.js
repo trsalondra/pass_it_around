6f4142233585ab2ceecffcfef2ef81b5e923b51b
trsalondra <trsalondra@gmail.com>
2022-11-30 13:44:52 -0600

    update pass it around

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
     res.send(`99 Bottles of beer on the wall<br><a href = http://localhost:${port}/98>take one down, pass it around</a> `)
     // res.put(bottleNum--)
 })
 
 app.get('/0', (req, res) => {
-    res.send(`0 Bottles of beer on the wall<br>`)
+    res.send(`0 Bottles of beer on the wall<br><a href = http://localhost:${port}/>take one down, pass it around</a>`)
 }) 
 
 app.get('/:bottleNum', (req, res) => {
